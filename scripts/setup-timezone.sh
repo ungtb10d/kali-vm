2cdefd5c803a8fc055c2c5fa41b74886fef6b53b
Arnaud Rebillout <arnaudr@kali.org>
2022-07-14 19:05:34 +0200

    Save timezone settings in the debconf database
    At the moment, here's what we have in the debconf database:
    
      # debconf-show tzdata | grep '^*'
      * tzdata/Areas: Etc
      * tzdata/Zones/Etc: UTC
    
    However this is not correct, as the defaults are US/Eastern.
    
    With this commit, after configuring the system, we save the values in
    the debconf database. And here's how it looks like as a result:
    
      # debconf-show tzdata | grep '^*'
      * tzdata/Areas: America
      * tzdata/Zones/America: New_York
      * tzdata/Zones/Etc: UTC
    
    Note that America/New_York is equivalent to US/Eastern, as we can see if
    we resolve the symlink /usr/share/zoneinfo/US/Eastern.  Interestingly,
    calling dpkg-reconfigure doesn't only update the database, it seems
    that it also updates the files in /etc, as they now point to
    America/New_York instead of US/Eastern:
    
      # cat /etc/timezone
      America/New_York
      # readlink /etc/localtime
      /usr/share/zoneinfo/America/New_York
    
    Also, note that I tried another approach: to simply use the existing
    script scripts/preseed.sh to pre-configure the system before we install
    tzdata.  However tzdata is already installed when the preseed.sh script
    runs (it's installed by debootstrap), so answering the debconf questions
    at this point is too late. Then I tried running `dpkg-reconfigure
    tzdata` to "apply" the new values that I just updated in the debconf
    database, but what happens is exactly the opposite: instead
    dpkg-reconfigure picks the values from the files in /etc, and update the
    values in the database based on that. Not sure if it's a bug or the
    expected behavior, cf. this comment that doesn't really answer:
    <https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=352697#27>

--- scripts/setup-timezone.sh
+++ scripts/setup-timezone.sh
@@ -12,3 +12,8 @@ fi
 echo "$zone" > /etc/timezone
 rm -f /etc/localtime
 ln -s /usr/share/zoneinfo/$zone /etc/localtime
+
+# Save settings in the debconf database
+DEBIAN_FRONTEND=noninteractive \
+DEBCONF_NONINTERACTIVE_SEEN=true \
+    dpkg-reconfigure tzdata
