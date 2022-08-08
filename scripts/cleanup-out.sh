b266e92ff330ff653f473747d0c21d6f7eae4df5
Arnaud Rebillout <arnaudr@kali.org>
2022-08-08 10:31:48 +0200

    Remove /var/lib/apt/lists in the images
    I thought it was done by apt-get clean but I was wrong

--- scripts/cleanup-out.sh
+++ scripts/cleanup-out.sh
@@ -9,12 +9,10 @@ fi
 
 rm -f  $ROOTDIR/etc/ssh/ssh_host_*
 rm -fr $ROOTDIR/tmp/*
+rm -fr $ROOTDIR/var/lib/apt/lists/*
 rm -f  $ROOTDIR/var/log/bootstrap.log
 rm -fr $ROOTDIR/var/tmp/*
 
 # Taken from kali-docker, however not sure it's suitable here,
-# and we already run 'apt-get clean' anyway, so just don't.
 #rm -f  $ROOTDIR/var/cache/ldconfig/aux-cache
-#rm -rf $ROOTDIR/var/lib/apt/lists/*
-#mkdir  $ROOTDIR/var/lib/apt/lists/partial
 #find   $ROOTDIR/var/log -depth -type f -print0 | xargs -0 truncate -s 0
