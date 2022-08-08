bb835931c12b27fc08cda2b54afabc6f54963680
Arnaud Rebillout <arnaudr@kali.org>
2022-08-08 10:16:58 +0200

    Rework the cleanup-in.sh script
    To list rc packages: squeezing whitespaces is a bit more robust than
    assuming exactly two spaces after ^rc.
    
    To purge rc packages: let's use apt-get rather than dpkg. Actually apt
    does more than dpkg, it also removes dependencies of the packages that
    are removed during the transaction, if ever those dependencies are not
    needed anymore.
    
    Also, no need for a loop, we can pass all the packages at once.

--- scripts/cleanup-in.sh
+++ scripts/cleanup-in.sh
@@ -4,10 +4,7 @@ set -eu
 
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get autoremove --purge -y
+apt-get autoremove -y --purge
+rc_packages=$(dpkg --list | grep "^rc" | tr -s " " | cut -d " " -f 2)
+apt-get purge -y $rc_packages
 apt-get clean
-
-rc_packages=$(dpkg --list | grep "^rc" | cut -d " " -f 3)
-for pkg in $rc_packages; do
-    dpkg --purge $pkg
-done
